e41a6277f69972ce7c8c0742c354c7c9eb4d63ce
Pablo Sirera <psirera4@gmail.com>
2020-04-14 09:48:29 +0200

    remove vue logo

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home container mx-auto">
-    <img class="mx-auto" alt="Vue logo" src="@/assets/logo.png" />
     <button
       @click="goToCreate"
       class="mt-5 p-2 border-solid rounded border-black bg-blue-500 text-white font-bold hover:opacity-75"
