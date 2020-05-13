67be4ca28a0b677c304a4269871c336e82a08984
Pablo Sirera <psirera4@gmail.com>
2020-05-13 22:44:32 +0200

    feat(createSession): add logic to save session

--- src/store/modules/session/actions.js
+++ src/store/modules/session/actions.js
@@ -1,23 +1,23 @@
 import axios from 'axios'
+const mutations = require('@/graphql/mutations/mutations.graphql')
+const queries = require('@/graphql/queries/queries.graphql')
 
 export default {
   async listSessions({ commit }) {
-    const result = await axios({
-      url: 'https://who-reviews.herokuapp.com/v1/graphql',
-      method: 'post',
-      data: {
-        query: `
-        query AllSessions {
-          session {
-            id
-            name
-            active
-          }
-        }
-      `,
-      },
+    const result = await axios.post(process.env.VUE_APP_HASURA_GRAPHQL, {
+      query: queries.allSessions,
     })
     commit('setSessions', result.data.data.session)
     return result.data.data.session
+  },
+  async createSession(_, payload) {
+    const result = await axios.post(process.env.VUE_APP_HASURA_GRAPHQL, {
+      query: mutations.saveSession,
+      variables: { session: payload },
+    })
+    const hasErrors = result.data.errors && result.data.errors.length
+    if (!hasErrors) {
+      // TODO: show confirm toast
+    }
   },
 }
--- src/views/createSession/CreateSession.vue
+++ src/views/createSession/CreateSession.vue
@@ -1,14 +1,23 @@
 <template>
-  <CreateSessionForm />
+  <CreateSessionForm @submit-form="sendCreateSession" />
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import CreateSessionForm from '@/views/createSession/components/CreateSessionForm'
 
 export default {
   name: 'CreateSession',
   components: {
     CreateSessionForm,
+  },
+  methods: {
+    ...mapActions({
+      createSession: 'session/createSession',
+    }),
+    sendCreateSession(params) {
+      this.createSession(params)
+    },
   },
 }
 </script>
